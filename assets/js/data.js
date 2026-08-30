0444d6dca4bfd451c5b22c2847b0349dcbe2708f
giovannibenda <giovanni.lodovico.benda@gmail.com>
2026-08-31 00:03:58 +0200

    add localstorage

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,7 +4,7 @@
 let trackList = [];
 
 async function loadTrackList() {
-    const response = await fetch("static/json/videosDB.json");
+    const response = await fetch("assets/data/videosDB.json");
     trackList = await response.json();
     return trackList;
 }
